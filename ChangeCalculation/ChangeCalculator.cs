6a939ef175fa3a15cb050504f65aa69a3342cf22
Николай <kolya.strepetov@mail.ru>
2025-07-27 17:29:59 +0500

    feat: add function for change calculation

--- ChangeCalculation/ChangeCalculator.cs
+++ ChangeCalculation/ChangeCalculator.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace ChangeCalculation
+{
+    public static class ChangeCalculator
+    {
+        public static object CalculateChange(int change, List<int> availableCoins)
+        {
+            if (availableCoins.Contains(change))
+            {
+                return new List<int> { change };
+            }
+            else
+            {
+                return "Оплатите картой!";
+            }
+        }
+    }
+}
